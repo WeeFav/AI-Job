6d101982cfb6348baf3484ba10b9facc97290605
Marvin <marvinlim4th@gmail.com>
2025-06-09 23:22:37 +0800

    able to scroll jobright and get all listed jobs

--- jobright_scrape.py
+++ jobright_scrape.py
@@ -26,11 +26,27 @@ def main():
         )
         context = browser.new_context(storage_state="auth.json")
         page = context.new_page()
-        page.goto("https://jobright.ai/jobs/info/6843a5ecc28f21afd582aad4")
-        # page.goto("https://jobright.ai/jobs/recommend")
-        # page.click("xpath=//*[@id='6843a5ecc28f21afd582aad4']")
-        apply_url = page.locator("a.index_origin__7NnDG").get_attribute("href")
-        print(apply_url)
+        page.goto("https://jobright.ai/jobs/recommend")
+        
+        scroll_locator = page.locator("div#scrollableDiv")
+        scroll_locator.wait_for()
+        ul_locator = page.locator("ul.ant-list-items")
+        ul_locator.wait_for()
+        
+        scroll_locator.evaluate("(el) => { el.scrollTop = el.scrollHeight; }")
+        
+        time.sleep(1)
+        
+        divs = ul_locator.locator("xpath=/div")
+        for i in range(divs.count()):
+            print(divs.nth(i).get_attribute("id"))
+        
+        
+        
+        # # page.goto("https://jobright.ai/jobs/recommend")
+        # # page.click("xpath=//*[@id='6843a5ecc28f21afd582aad4']")
+        # apply_url = page.locator("a.index_origin__7NnDG").get_attribute("href")
+        # print(apply_url)
         
 if __name__ == '__main__':
     main()
