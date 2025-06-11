38fced5d185232fbb6e0f9e98d0a93b65baac707
Marvin <marvinlim4th@gmail.com>
2025-06-11 23:44:27 +0800

    can scrape jobright

--- jobright_scrape.py
+++ jobright_scrape.py
@@ -1,6 +1,9 @@
 from playwright.sync_api import sync_playwright
 import time
 
+jobs_to_scrape = 10
+jobs_per_page = 10
+
 def get_auth():
     """Only need when need to sign into google"""
     with sync_playwright() as playwright:
@@ -20,33 +23,41 @@ def get_auth():
     
 def main():
     with sync_playwright() as playwright:
+        # open browser and navigate to jobright
         browser = playwright.chromium.launch(
             channel="chrome",
             headless=False,
         )
         context = browser.new_context(storage_state="auth.json")
         page = context.new_page()
+
         page.goto("https://jobright.ai/jobs/recommend")
         
+        # scroll until all jobs are visible
         scroll_locator = page.locator("div#scrollableDiv")
         scroll_locator.wait_for()
         ul_locator = page.locator("ul.ant-list-items")
         ul_locator.wait_for()
         
-        scroll_locator.evaluate("(el) => { el.scrollTop = el.scrollHeight; }")
+        for i in range(jobs_to_scrape // jobs_per_page):
+            scroll_locator.evaluate("(el) => { el.scrollTop = el.scrollHeight; }")
+            time.sleep(2) # wait for jobs to load
         
-        time.sleep(1)
-        
+        # get job list
         divs = ul_locator.locator("xpath=/div")
-        for i in range(divs.count()):
-            print(divs.nth(i).get_attribute("id"))
+        job_ids = [divs.nth(i).get_attribute("id") for i in range(divs.count())]
+        # scape each job
+        for job_id in job_ids:
+            page.goto(f"https://jobright.ai/jobs/info/{job_id}")
+            company = page.locator("h2.index_company-row__vOzgg").inner_text().split("\n")[0]
+            title = page.locator("h1.index_job-title__sStdA").text_content()
+            apply_url = page.locator("a.index_origin__7NnDG").get_attribute("href")
+            print("Company:", company)
+            print("Title:", title)
+            print("URL: ", apply_url)
         
-        
-        
-        # # page.goto("https://jobright.ai/jobs/recommend")
-        # # page.click("xpath=//*[@id='6843a5ecc28f21afd582aad4']")
-        # apply_url = page.locator("a.index_origin__7NnDG").get_attribute("href")
-        # print(apply_url)
+        # page.click("xpath=//*[@id='6843a5ecc28f21afd582aad4']")
+
         
 if __name__ == '__main__':
     main()
